bc6c3aa58f046a57e36f3ef5a74a7be92a8636b9
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2025-04-13 13:18:49 +0330

    validated auth routes using express validator

--- src/routes/auth/index.js
+++ src/routes/auth/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const controler = require("./controler");
+const validator = require("./validator");
 
-router.post("/login", controler.login);
-router.post("/register", controler.register);
+router.post("/login", validator.loginValidator(), controler.login);
+router.post("/register", validator.registerValidator(), controler.register);
 
 module.exports = router;
--- src/routes/auth/validator.js
+++ src/routes/auth/validator.js
@@ -1,2 +1,39 @@
 const expressValidator = require("express-validator");
-const check=expressValidator
+const check = expressValidator.check;
+
+module.exports = new (class {
+  registerValidator() {
+    return [
+      check("username")
+        .notEmpty()
+        .withMessage("نام کاربری یا رمز عبور نمی تواند خالی باشد.")
+        .isString()
+        .withMessage("نام کاربری باید یک رشته باشد.")
+        .isLength({ min: 5, max: 14 })
+        .withMessage("نام کاربری باید بین 5 تا 14 کاراکتر باشد.")
+        .matches(/^[A-Za-z]+$/)
+        .withMessage("نام کاربری باید فقط شامل حروف انگلیسی باشد."),
+      check("password")
+        .isString()
+        .withMessage("رمز عبور باید یک رشته باشد.")
+        .isLength({ min: 8, max: 16 })
+        .withMessage("رمز عبور باید بین 5 تا 14 کاراکتر باشد.")
+        .matches("/^(?=.*[a-z])(?=.*[A-Z])(?=.*d).{6,}$/")
+        .withMessage(
+          "رمز عبور باید شامل حداقل یک حرف بزرگ، یک حرف کوچک و یک عدد باشد."
+        )
+        .notEmpty()
+        .withMessage("نام کاربری یا رمز عبور نمی تواند خالی باشد."),
+    ];
+  }
+  loginValidator() {
+    return [
+      check("username")
+        .notEmpty()
+        .withMessage("نام کاربری یا رمز عبور نمی تواند خالی باشد."),
+      check("password")
+        .notEmpty()
+        .withMessage("نام کاربری یا رمز عبور نمی تواند خالی باشد."),
+    ];
+  }
+})();
